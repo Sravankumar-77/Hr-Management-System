2249d92626a3b78f9b549566c81a5f4958a23e7b
Sravankumar-77 <nandyalasravankumar777@gmail.com>
2025-12-16 11:04:10 +0530

    Add files via upload

---
+++ accounts/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Student(models.Model):
+    user = models.ForeignKey(User,on_delete = models.PROTECT)
+    name = models.CharField(max_length=225)
+    email = models.EmailField(max_length=255,unique=True)
+    education = models.CharField(max_length=255)
+    course = models.CharField(max_length=225)
+    admission_date = models.DateField(auto_now_add=True)
+    paid_fee = models.DecimalField(max_digits=7,decimal_places=2)
+    total_fee = models.DecimalField(max_digits=7,decimal_places=2)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        db_table = 'student'
